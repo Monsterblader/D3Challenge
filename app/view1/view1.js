08447f4868e64ad8bcc1b0b4eea4c9b3c625d443
Monsterblader <Monsterblader@gmail.com>
2015-04-21 21:47:57 -0700

    (Refactor) Replace all $watch with functions.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,113 +9,109 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-        .controller('View1Ctrl', ['$scope', function ($scope) {
-            $scope.parsedData;
+// TODO properly render default values on load
+// TODO always show all segments
+// TODO mean line and trend line
+// TODO color category names on pie chart
+// TODO animate changes to chart
+// TODO fix mean line for one day
+// TODO implement geolocation?
+    .controller('View1Ctrl', ['$scope', function ($scope) {
+            $scope.parsedData = {};
             $scope.devices = {mobile: 10, desktop: 1, tablet: 1};
-            var pie,
-                    pieParams = {
-                      "size": {
-                        "canvasHeight": 300,
-                        "canvasWidth": 300
-                      },
-                      "data": {
-                        "sortOrder": "value-asc",
-                        "content": [
-                          {
-                            "label": "Mobile",
-                            "value": $scope.devices.mobile,
-                            "color": "RGB(201, 218, 248)"
-                          },
-                          {
-                            "label": "Desktop",
-                            "value": $scope.devices.desktop,
-                            "color": "RGB(234, 208, 220)"
-                          },
-                          {
-                            "label": "Tablet",
-                            "value": $scope.devices.tablet,
-                            "color": "RGB(252, 229, 204)"
-                          }
-                        ]
-                      },
-                      "labels": {
-                        "outer": {
-                          "pieDistance": 32
-                        },
-                        "inner": {
-                          "hideWhenLessThanPercentage": 100
-                        },
-                        "mainLabel": {
-                          "fontSize": 11
-                        },
-                        "percentage": {
-                          "color": "#ffffff",
-                          "decimalPlaces": 0
-                        },
-                        "value": {
-                          "color": "#adadad",
-                          "fontSize": 11
-                        },
-                        "lines": {
-                          "enabled": true
-                        }
-                      },
-                      "effects": {
-                        "pullOutSegmentOnClick": {
-                          "effect": "linear",
-                          "speed": 400,
-                          "size": 8
-                        },
-                        "highlightSegmentOnMouseover": false
-                      },
-                      "misc": {
-                        "pieCenterOffset": {
-                          "x": 50,
-                          "y": -10
-                        }
-                      }
-                    };
+            var pie;
+            var pieParams = {
+              "size": {
+                "canvasHeight": 300,
+                "canvasWidth": 300
+              },
+              "data": {
+                "sortOrder": "value-asc",
+                "content": [
+                  {
+                    "label": "Mobile",
+                    "value": $scope.devices.mobile,
+                    "color": "RGB(201, 218, 248)"
+                  },
+                  {
+                    "label": "Desktop",
+                    "value": $scope.devices.desktop,
+                    "color": "RGB(234, 208, 220)"
+                  },
+                  {
+                    "label": "Tablet",
+                    "value": $scope.devices.tablet,
+                    "color": "RGB(252, 229, 204)"
+                  }
+                ]
+              },
+              "labels": {
+                "outer": {
+                  "pieDistance": 32
+                },
+                "inner": {
+                  "hideWhenLessThanPercentage": 100
+                },
+                "mainLabel": {
+                  "fontSize": 11
+                },
+                "percentage": {
+                  "color": "#ffffff",
+                  "decimalPlaces": 0
+                },
+                "value": {
+                  "color": "#adadad",
+                  "fontSize": 11
+                },
+                "lines": {
+                  "enabled": true
+                }
+              },
+              "effects": {
+                "pullOutSegmentOnClick": {
+                  "effect": "linear",
+                  "speed": 400,
+                  "size": 8
+                },
+                "highlightSegmentOnMouseover": false
+              },
+              "misc": {
+                "pieCenterOffset": {
+                  "x": 50,
+                  "y": -10
+                }
+              }
+            };
 
             var filterData = function (data, scope) {
-              var output = [],
-                      today = new Date('7/31/2014 23:59:59 GMT-0700 (PDT)'),
-                      parseDate = d3.time.format("%x").parse,
-                      filteredData = _.filter(data, function (element) {
-                        var inTimeRange = today - new Date(element.Date + " " + element.Time) < scope.timeRange * 86400000;
-                        debugger;
-                        return inTimeRange && scope.segment.gender === "all" || scope.segment.gender === element.Gender;
-                      });
+              var output = [];
+              var today = new Date('7/31/2014 23:59:59 GMT-0700 (PDT)');
+              var parseDate = d3.time.format("%x").parse;
+              var filteredData = _.filter(data, function (element) {
+                var inTimeRange = today - new Date(element.Date + " " + element.Time) < scope.timeRange * 86400000;
+                return +element.Activity && inTimeRange && scope.segment.gender === "all" || scope.segment.gender === element.Gender;
+              });
               scope.segment.genderCount = _.countBy(filteredData, function (element) {
                 return element.Gender;
               });
               scope.devices = _.countBy(filteredData, function (element) {
                 return element.Device;
               });
-              filteredData = _.countBy(filteredData, function (element) {
-                return element.Date;
-              });
-              for (var key in filteredData) {
-                output.push({Date: parseDate(key), Activity: filteredData[key] / data.length * 100});
+              if ($scope.lineType === "mean") {
+                filteredData = _.countBy(filteredData, function (element) {
+                  return element.Date;
+                });
+                for (var key in filteredData) {
+                  output.push({Date: parseDate(key), Activity: filteredData[key] / data.length * 100});
+                }
               }
               return _.sortBy(output, function (element) {
                 return element.Date;
               });
               // if trend line, do not count by, sort by Date + Time
             };
-            /*
-             * on load
-             *  draw axes.
-             *  set filters - all/male/female, mean/trend, all/14/7/3 days
-             *  create draw algorithm that checks filters
-             *  add transitions
-             *
-             */
 
             $scope.init = function () {
-              d3.csv("SampleData.csv", function (data) {
-                $scope.parsedData = data;
-              });
-              $scope.renderCharts($scope.parsedData, $scope);
             };
 
             $scope.renderCharts = function (data) {
@@ -181,20 +177,38 @@ angular.module('myApp.view1', ['ngRoute'])
                       .attr("d", line);
             };
 
-            $scope.$watch("devices", function (newvalue, oldvalue, scope) {
-              pieParams.data.content[0].value = scope.devices.mobile;
-              pieParams.data.content[1].value = scope.devices.desktop;
-              pieParams.data.content[2].value = scope.devices.tablet;
+        $scope.renderPie = function (scope) {
+          pieParams.data.content[0].value = scope.devices.mobile;
+          pieParams.data.content[1].value = scope.devices.desktop;
+          pieParams.data.content[2].value = scope.devices.tablet;
 
-              pie && pie.destroy();
-              pie = new d3pie("pieChart", pieParams);
-            });
+          pie && pie.destroy();
+          pie = new d3pie("pieChart", pieParams);
+        };
 
-            $scope.$watch("timeRange", function (newvalue, oldvalue, scope) {
-              $scope.renderCharts($scope.parsedData, $scope);
-            });
+        $scope.setLineType = function (type) {
+          $scope.lineType = type;
+          $scope.renderCharts($scope.parsedData, $scope);
+          $scope.renderPie($scope);
+        };
 
-            $scope.$watch("segment.gender", function (newvalue, oldvalue, scope) {
-              $scope.renderCharts($scope.parsedData, $scope);
-            });
+        $scope.setTimeRange = function (range) {
+          $scope.timeRange = range;
+          $scope.renderCharts($scope.parsedData, $scope);
+          $scope.renderPie($scope);
+        };
+
+        $scope.setGender = function (gender) {
+          $scope.segment.gender = gender;
+          $scope.renderCharts($scope.parsedData, $scope);
+          $scope.renderPie($scope);
+        };
+
+        $scope.$on("$viewContentLoaded", function () {
+          d3.csv("SampleData.csv", function (data) {
+            $scope.parsedData = data;
+            $scope.renderCharts($scope.parsedData, $scope);
+            $scope.renderPie($scope);
+          });
+        });
 }]);
